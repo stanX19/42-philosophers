242a140d5eb55be892c4c85ff3f323b60cf7dda0
StanX19 <tanshanchien@gmail.com>
2024-07-21 21:58:50 +0800

    data struct ready?

--- philo/main.c
+++ philo/main.c
@@ -1,16 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stan <shatan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 21:15:30 by stan              #+#    #+#             */
+/*   Updated: 2024/07/21 21:58:37 by stan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+
+void	delete_data(t_data *data)
+{
+	if (data->fork_arr)
+		free(data->fork_arr);
+	if (data->thread_arr)
+		free(data->thread_arr);
+	if (data->philo_arr)
+		free(data->philo_arr);
+}
+
+static t_philo	*create_philo_arr(int count, t_vars *vars,
+		t_mutex *forks)
+{
+	t_philo	*ret;
+	int		i;
+
+	if (count == 0 || vars == NULL || forks == NULL)
+		return (NULL);
+	ret = (t_philo *)malloc(sizeof(t_philo) * count);
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		ret[i].status = S_THINKING;
+		ret[i].last_eat = 0;
+		ret[i].vars = vars;
+		ret[i].left_fork = forks + i;
+		ret[i].right_fork = forks + (i + 1) % count;
+		i++;
+	}
+	return (ret);
+}
 
 int	init_data(t_data *data, int argc, char *const *argv)
 {
-	data->philo_count = ft_atou(argv[1], ULLONG_MAX);
-	data->death_time = (u_int64_t)ft_atou(argv[2], ULLONG_MAX);
-	data->eat_time = (u_int64_t)ft_atou(argv[3], ULLONG_MAX);
-	data->sleep_time = (u_int64_t)ft_atou(argv[4], ULLONG_MAX);
-	data->eat_needed = -1;
+	data->philo_count = ft_atou(argv[1], INT_MAX);
+	data->vars.death_time = (t_time)ft_atou(argv[2], ULLONG_MAX);
+	data->vars.eat_time = (t_time)ft_atou(argv[3], ULLONG_MAX);
+	data->vars.sleep_time = (t_time)ft_atou(argv[4], ULLONG_MAX);
+	data->vars.eat_needed = -1;
 	if (argc == 6)
-		data->eat_needed = ft_atou(argv[5], LLONG_MAX);
+		data->vars.eat_needed = ft_atou(argv[5], LLONG_MAX);
+	data->thread_arr = (t_thread *)malloc(sizeof(t_thread) * data->philo_count);
+	data->fork_arr = (t_mutex *)malloc(sizeof(t_mutex) * data->philo_count);
+	data->philo_arr = create_philo_arr(data->philo_count, &data->vars,
+			data->fork_arr);
 	if (errno)
+	{
+		delete_data(data);
 		return (1);
+	}
 	return (0);
 }
 
@@ -22,5 +75,6 @@ int	main(int argc, char *const *argv)
 		return (1);
 	if (init_data(&data, argc, argv))
 		return (1);
+	delete_data(&data);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shatan <shatan@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: stan <shatan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:10:39 by shatan            #+#    #+#             */
-/*   Updated: 2024/07/01 17:20:42 by shatan           ###   ########.fr       */
+/*   Updated: 2024/07/21 21:49:34 by stan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,49 @@
 # include <sys/time.h>
 # include <unistd.h>
 
+# ifdef __APPLE__
+  typedef u_int64_t t_time;
+# else
+  typedef suseconds_t t_time;
+# endif
+typedef pthread_mutex_t t_mutex;
+typedef pthread_t t_thread;
+
+typedef enum e_status
+{
+	S_EATING,
+	S_THINKING,
+	S_SLEEPING,
+	S_DEAD,
+}	t_status;
+
+typedef struct s_vars
+{
+	t_time				death_time;
+	t_time				eat_time;
+	t_time				sleep_time;
+	t_time				start_time;
+	long long			eat_needed;
+}	t_vars;
+
+
+typedef struct s_philo
+{
+	t_status	status;
+	t_time		last_eat;
+	t_mutex		*left_fork;
+	t_mutex		*right_fork;
+	t_vars		*vars;
+}	t_philo;
+
+
 typedef struct s_data
 {
-	unsigned long long	philo_count;
-	u_int64_t			death_time;
-	u_int64_t			eat_time;
-	u_int64_t			sleep_time;
-	u_int64_t			start_time;
-	long long			eat_needed;
+	int					philo_count;
+	t_vars				vars;
+	t_thread			*thread_arr;
+	t_philo				*philo_arr;
+	t_mutex				*fork_arr;
 }						t_data;
 
 unsigned long long	ft_atou(const char *nptr, unsigned long long limit);
